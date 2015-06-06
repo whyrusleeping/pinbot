6699340e46dc7227b77f7919925a37d7789c16ba
Jeromy <jeromyj@gmail.com>
2015-06-06 09:37:11 -0700

    give zignig acccess

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	shell "github.com/whyrusleeping/ipfs-shell"
 )
 
-var friends = []string{"whyrusleeping", "jbenet", "tperson", "krl", "kyledrake"}
+var friends = []string{"whyrusleeping", "jbenet", "tperson", "krl", "kyledrake", "zignig"}
 
 func isFriend(name string) bool {
 	for _, n := range friends {
